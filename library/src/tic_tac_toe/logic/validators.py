b323dfa4ffaf281e146e6c4b3cff5bfa5d7d8f75
PinkPaca <fmadkdl113@gmail.com>
2023-05-08 11:16:41 +0100

    Model the Tic-Ta-Toe Game Domain

--- library/src/tic_tac_toe/logic/validators.py
+++ library/src/tic_tac_toe/logic/validators.py
@@ -15,3 +15,33 @@ def validate_game_state(game_state: GameState) -> None:
     validate_winner(
         game_state.grid, game_state.starting_mark, game_state.winner
     )
+
+def validate_number_of_marks(grid: Grid) -> None:
+    if abs(grid.x_count - o_count) > 1:
+        raise InvalidGameState("Wrong number of Xs and Os")
+    
+def validate_starting_mark(grid: Grid, starting_mark: Mark) -> None:
+    if grid.x_count > grid.o_count:
+        if starting_mark != "X":
+            raise InvalidGameState("Wrong starting mark")
+        elif grid.o_count > grid.x_count:
+            if starting_mark != "O":
+                raise InvalidGameState("Wrong starting mark")
+            
+def validate_winner(
+        grid: Grid, starting_mark: Mark, winner: Mark | None
+) -> None:
+    if winner == "X":
+        if starting_mark == "X":
+            if grid.x_count <= grid.o_count:
+                raise InvalidGameState("Wrong number of Xs")
+            else:
+                if grid.x_count != grid.o_count:
+                    raise InvalidGameState("Wrong number of Xs")
+    elif winner == "O":
+        if starting_mark == "O":
+            if grid.o_count <= grid.x_count:
+                raise InvalidGameState("Wrong number of Os")
+            else:
+                if grid.o_count != grid.x_count:
+                    raise InvalidGameState("Wrong number of Os")

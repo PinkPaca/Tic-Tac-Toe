b474139a1d6e7fe6334d00416a128db7c256f697
PinkPaca <fmadkdl113@gmail.com>
2023-05-09 16:11:13 +0100

    Scaffold

--- library/src/tic_tac_toe/game/renderers.py
+++ library/src/tic_tac_toe/game/renderers.py
@@ -1,1 +1,10 @@
 #interface
+
+import abc
+
+from tic_tac_toe.logic.models import GameState
+
+class Renderer(metaclass=abc.ABCMeta):
+    @abc.abstractmethod
+    def render(self, game_state: GameState) -> None:
+        """Render the current game state."""
